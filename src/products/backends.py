f3192d856fff57db6716f1a62bae029dcc5fa92a
Nail Gimaev <gimntut@list.ru>
2025-07-04 22:03:47 +0500

    Ruff format

--- src/products/backends.py
+++ src/products/backends.py
@@ -2,7 +2,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 
 class FilterBackend(DjangoFilterBackend):
-
     def get_filterset_kwargs(self, request, queryset, view):
         return {
             "data": request.GET,
